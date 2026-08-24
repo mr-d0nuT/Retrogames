85caf4f0587534d30fee0aa06c8af2f23eeae804
mr-d0nuT <94043977+mr-d0nuT@users.noreply.github.com>
2026-08-24 21:56:32 +0200

    Reparent virtual gamepad buttons (start, select, fast, slow) directly to the main container via JS to break them out of transformed scaling containers, guaranteeing they anchor to the absolute top of the screen

--- arcade/js/app.js
+++ arcade/js/app.js
@@ -162,7 +162,7 @@ document.addEventListener("DOMContentLoaded", () => {
             
                 
                                                                                                 /* Virtual gamepad central top positioning */
-                .b_select, .b_start, .b_speed_slow, .b_speed_fast { position: fixed !important; top: 15px !important; bottom: auto !important; margin: 0 !important; transform: none !important; }
+                .b_select, .b_start, .b_speed_slow, .b_speed_fast { position: fixed !important; top: 15px !important; bottom: auto !important; margin: 0 !important; transform: none !important; z-index: 9999 !important; }
                 .b_select { left: calc(50vw - 150px) !important; right: auto !important; }
                 .b_start { left: calc(50vw - 50px) !important; right: auto !important; }
                 .b_speed_slow { left: calc(50vw + 50px) !important; right: auto !important; }
@@ -187,6 +187,25 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.EJS_backgroundImage = window.location.origin + window.location.pathname.replace('index.html', '') + '${game.cover}';
                 window.EJS_backgroundBlur = true;
                 window.EJS_pathtodata = '../emulatorjs/data/';
+            
+                // Reparent virtual gamepad buttons to break out of scaled containers
+                let checkButtons = setInterval(() => {
+                    const selectBtn = document.querySelector('.b_select');
+                    const startBtn = document.querySelector('.b_start');
+                    const slowBtn = document.querySelector('.b_speed_slow');
+                    const fastBtn = document.querySelector('.b_speed_fast');
+                    const gameContainer = document.querySelector('#game');
+                    
+                    if (startBtn && selectBtn && gameContainer) {
+                        clearInterval(checkButtons);
+                        [selectBtn, startBtn, slowBtn, fastBtn].forEach(btn => {
+                            if (btn) {
+                                btn.style.zIndex = '9999';
+                                gameContainer.appendChild(btn);
+                            }
+                        });
+                    }
+                }, 500);
             </script>
             <script src="../emulatorjs/data/loader.js"></script>
         </body>
